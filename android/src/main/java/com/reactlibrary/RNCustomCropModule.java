5e29590293ac33c4e919b728ac7b32eb8536b0b4
Raghav Rawat <raghav39@gmail.com>
2018-02-22 10:01:03 +0530

    adding proper name and method for navtive module

--- android/src/main/java/com/reactlibrary/RNCustomCropModule.java
+++ android/src/main/java/com/reactlibrary/RNCustomCropModule.java
@@ -1,10 +1,13 @@
 
 package com.reactlibrary;
 
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
 
 public class RNCustomCropModule extends ReactContextBaseJavaModule {
 
@@ -17,6 +20,16 @@ public class RNCustomCropModule extends ReactContextBaseJavaModule {
 
   @Override
   public String getName() {
-    return "RNCustomCrop";
+    return "CustomCropManager";
+  }
+
+  @ReactMethod
+  public void crop(ReadableMap points, String base64Image, Callback successCallBack){
+      try {
+          Toast.makeText(reactContext, "should crop now", Toast.LENGTH_LONG).show();
+          successCallBack.invoke(null, base64Image);
+      } catch (Exception e) {
+          Toast.makeText(reactContext, "Unable to crop the image", Toast.LENGTH_LONG).show();
+      }
   }
 }
